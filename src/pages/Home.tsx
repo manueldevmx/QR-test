6a69aa7c48ca71998a21e9330e51602110ca53f9
hack_dev <manueldevmx@gmail.com>
2021-04-21 20:24:12 -0500

    QRScanner - test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,44 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import React from 'react';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton
+
+} from '@ionic/react';
+
+//import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { Plugins } from '@capacitor/core';
+
+
+
 
 const Home: React.FC = () => {
+ 
+  const startScan = async () => {
+    const { BarcodeScanner } = Plugins;
+
+    BarcodeScanner.hideBackground(); //Funcion para hacer transparente el background webview
+
+    const result = await BarcodeScanner.startScan(); //Empieza a scanear y espera el resultado
+
+    //si el resultado tiene contenido
+    if (result.hasContent) {
+      console.log(result.content); // muestra el  conetnido scaneado
+    }
+
+  }
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Tab 1</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent>
+        <IonButton onClick={startScan}>Scan barcode</IonButton>
       </IonContent>
     </IonPage>
   );
